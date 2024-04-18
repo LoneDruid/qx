24c1f0f2a9bf5cb20e2ec0786367c897f5e832b9
LD <mufeng.me@outlook.com>
2024-04-18 08:29:37 +0800

    Add files via upload

---
+++ jk.js
@@ -0,0 +1,29 @@
+const url = $request.url;
+const method = $request.method;
+
+if (!$response.body) {
+  console.log(`$response.body is undefined: ${url}`);
+  $done({});
+}
+
+if (method !== "GET") {
+  console.log("Invalid method:", method);
+}
+
+let body = JSON.parse($response.body);
+
+if (body.data) {
+  body.data.practice_top_no_ad_time = "999999"
+  body.data.jk_vip_donate = "true"
+  body.data.jk_vip_video_count = "{\n\t\"examVideoCount\": \"999999\",\n        \"practiceVideoCount\": \"999999\",\n\t\"explainVideoCount\": \"999999\"\n}"
+  body.data.more_advert_urls = "[]"
+  body.data.advert_pay_alert = "[]"
+  body.data.mcbd_clue_show_dealer = "true"
+  body.data.asgard_allow_user_authentication = "true"
+}
+
+body = JSON.stringify(body);
+
+$done({
+  body
+});
